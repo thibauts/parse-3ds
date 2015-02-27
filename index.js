ca6f7b09e28b4b0eec3c05e6e4f46c581a2430e4
thibauts <thibaut.seguy@gmail.com>
2015-02-28 00:45:02 +0100

    fixed tab settings

--- index.js
+++ index.js
@@ -6,154 +6,154 @@ var objectAssign = require('object-assign');
 
 // Those are parseable as a straight list of subchunks. 
 var NON_LEAF_CHUNKS = [
-	0x4D4D, // Main Chunk
-	0x3D3D, // 3D Editor Chunk
-	0x4100, // Triangular Mesh
-	0xAFFF, // Material Block
-	0xB000  // Keyframer Chunk
+  0x4D4D, // Main Chunk
+  0x3D3D, // 3D Editor Chunk
+  0x4100, // Triangular Mesh
+  0xAFFF, // Material Block
+  0xB000  // Keyframer Chunk
 ];
 
 // Add here parsers for other chunk types
 var CHUNK_PARSERS = {
-	0x4000: parseObjectChunk,
-	0x4110: parseVertexListChunk,
-	0x4120: parseFaceListChunk
+  0x4000: parseObjectChunk,
+  0x4110: parseVertexListChunk,
+  0x4120: parseFaceListChunk
 };
 
 var CHUNK_NAMES = {
-	0x4D4D: 'Main Chunk',
-	0x0002: 'Version',
-	0x3D3D: '3D Editor Chunk',
-	0x4000: 'Object Block',
-	0x4100: 'Triangular Mesh',
-	0x4110: 'Vertex List',
-	0x4120: 'Face List',
-	0x4130: 'Faces Material',
-	0x4150: 'Smoothing Group List',
-	0x4140: 'Mapping Coordinates List',
-	0x4160: 'Local Coordinates System',
-	0x4600: 'Light',
-	0x4610: 'Spotlight',
-	0x4700: 'Camera',
-	0xAFFF: 'Material Block',
-	0xA000: 'Material Name',
-	0xA010: 'Ambient Color',
-	0xA020: 'Diffuse Color',
-	0xA030: 'Specular Color',
-	0xA200: 'Texture Map 1',
-	0xA230: 'Bump Map',
-	0xA220: 'Reflection Map',
-	0xA300: 'Mapping Filename',
-	0xA351: 'Mapping Parameters',
-	0xB000: 'Keyframer Chunk',
-	0xB002: 'Mesh Information Block',
-	0xB007: 'Spot Light Information Block',
-	0xB008: 'Frames (Start and End)',
-	0xB010: 'Object Name',
-	0xB013: 'Object Pivot Point',
-	0xB020: 'Position Track',
-	0xB021: 'Rotation Track',
-	0xB022: 'Scale Track',
-	0xB030: 'Hierarchy Position'
+  0x4D4D: 'Main Chunk',
+  0x0002: 'Version',
+  0x3D3D: '3D Editor Chunk',
+  0x4000: 'Object Block',
+  0x4100: 'Triangular Mesh',
+  0x4110: 'Vertex List',
+  0x4120: 'Face List',
+  0x4130: 'Faces Material',
+  0x4150: 'Smoothing Group List',
+  0x4140: 'Mapping Coordinates List',
+  0x4160: 'Local Coordinates System',
+  0x4600: 'Light',
+  0x4610: 'Spotlight',
+  0x4700: 'Camera',
+  0xAFFF: 'Material Block',
+  0xA000: 'Material Name',
+  0xA010: 'Ambient Color',
+  0xA020: 'Diffuse Color',
+  0xA030: 'Specular Color',
+  0xA200: 'Texture Map 1',
+  0xA230: 'Bump Map',
+  0xA220: 'Reflection Map',
+  0xA300: 'Mapping Filename',
+  0xA351: 'Mapping Parameters',
+  0xB000: 'Keyframer Chunk',
+  0xB002: 'Mesh Information Block',
+  0xB007: 'Spot Light Information Block',
+  0xB008: 'Frames (Start and End)',
+  0xB010: 'Object Name',
+  0xB013: 'Object Pivot Point',
+  0xB020: 'Position Track',
+  0xB021: 'Rotation Track',
+  0xB022: 'Scale Track',
+  0xB030: 'Hierarchy Position'
 };
 
 
 function parseFaceListChunk(buf) {
-	var faceCount = buf.readUInt16LE();
+  var faceCount = buf.readUInt16LE();
 
-	// The face array contains 3 vertex indices + a 2 bytes 
-	// bit-field containing various flags (see [1]).
-	// The flags don't look very useful for now, so let's remove them
-	// and return a directly usable buffer instead.
+  // The face array contains 3 vertex indices + a 2 bytes 
+  // bit-field containing various flags (see [1]).
+  // The flags don't look very useful for now, so let's remove them
+  // and return a directly usable buffer instead.
 
-	var data = buf.slice(2);
-	var faces = [];
+  var data = buf.slice(2);
+  var faces = [];
 
-	for(var i=0; i<faceCount; i++) {
-		var off = i * 2 * 4;
-		faces.push(data.slice(off, off + 2 * 3));
-	}
+  for(var i=0; i<faceCount; i++) {
+    var off = i * 2 * 4;
+    faces.push(data.slice(off, off + 2 * 3));
+  }
 
-	// The face indices are returned as an UInt16LE buffer 
-	return {
-		faceCount: faceCount,
-		faces: Buffer.concat(faces)
-	};
+  // The face indices are returned as an UInt16LE buffer 
+  return {
+    faceCount: faceCount,
+    faces: Buffer.concat(faces)
+  };
 }
 
 
 function parseVertexListChunk(buf) {
-	var vertexCount = buf.readUInt16LE();
-	var vertices = buf.slice(2);
+  var vertexCount = buf.readUInt16LE();
+  var vertices = buf.slice(2);
 
-	// The vertice coordinates are returned as a Float32LE buffer 
-	return {
-		vertexCount: vertexCount,
-		vertices: vertices
-	};
+  // The vertice coordinates are returned as a Float32LE buffer 
+  return {
+    vertexCount: vertexCount,
+    vertices: vertices
+  };
 }
 
 
 function parseObjectChunk(buf) {
-	// The object chunk starts with the object name
-	// as a zero terminated ASCII string
-	var i = 0;
-	while(buf[i] != 0) {
-		i++;
-	}
+  // The object chunk starts with the object name
+  // as a zero terminated ASCII string
+  var i = 0;
+  while(buf[i] != 0) {
+    i++;
+  }
 
-	var objectName = buf.slice(0, i).toString();
-	var data = buf.slice(i + 1);
+  var objectName = buf.slice(0, i).toString();
+  var data = buf.slice(i + 1);
 
-	return {
-		objectName: objectName,
-		children: parseChildren(data)
-	};
+  return {
+    objectName: objectName,
+    children: parseChildren(data)
+  };
 }
 
 
 function parseChildren(buf) {
-	var offset = 0;
-	var children = [];
+  var offset = 0;
+  var children = [];
 
-	while(offset < buf.length) {
-		var chunk = parseChunk(buf, offset);
-		children.push(chunk);
-		offset += chunk.length;
-	}
+  while(offset < buf.length) {
+    var chunk = parseChunk(buf, offset);
+    children.push(chunk);
+    offset += chunk.length;
+  }
 
-	return children;
+  return children;
 }
 
 
 function parseChunk(buf, offset) {
-	var chunkId = buf.readUInt16LE(offset);
-	var chunkLength = buf.readUInt32LE(offset + 2);
-	var data = buf.slice(offset + 6, offset + chunkLength);
+  var chunkId = buf.readUInt16LE(offset);
+  var chunkLength = buf.readUInt32LE(offset + 2);
+  var data = buf.slice(offset + 6, offset + chunkLength);
 
-	var chunkName = CHUNK_NAMES[chunkId] || 'Unknown';
+  var chunkName = CHUNK_NAMES[chunkId] || 'Unknown';
 
-	var chunk = {
-		id: chunkId,
-		name: chunkName,
-		length: chunkLength
-	};
+  var chunk = {
+    id: chunkId,
+    name: chunkName,
+    length: chunkLength
+  };
 
-	/*
-	 * If a parser is defined for this chunkId, use it.
-	 * Else if the chunk is known as non-leaf, try to parse it as a list of children chunks
-	 */
-	if(CHUNK_PARSERS[chunk.id]) {
-		var parsed = CHUNK_PARSERS[chunk.id](data);
-		chunk = objectAssign({}, chunk, parsed);
-	} else if(NON_LEAF_CHUNKS.indexOf(chunk.id) !== -1) {
-		chunk.children = parseChildren(data);
-		delete chunk['data'];
-	}
+  /*
+   * If a parser is defined for this chunkId, use it.
+   * Else if the chunk is known as non-leaf, try to parse it as a list of children chunks
+   */
+  if(CHUNK_PARSERS[chunk.id]) {
+    var parsed = CHUNK_PARSERS[chunk.id](data);
+    chunk = objectAssign({}, chunk, parsed);
+  } else if(NON_LEAF_CHUNKS.indexOf(chunk.id) !== -1) {
+    chunk.children = parseChildren(data);
+    delete chunk['data'];
+  }
 
-	return chunk;
+  return chunk;
 }
 
 module.exports = function(buf) {
-	return parseChunk(buf, 0);
+  return parseChunk(buf, 0);
 };
