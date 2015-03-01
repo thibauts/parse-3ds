ad556bcd64cab2a394ae26765f83d3189f7b68fa
thibauts <thibaut.seguy@gmail.com>
2015-03-01 17:20:40 +0100

    made output more user-friendly

--- index.js
+++ index.js
@@ -153,6 +153,75 @@ function parseChunk(buf, offset) {
   return chunk;
 }
 
+
+function getChildChunk(tree, id) {
+  var chunks = getChildrenChunks(tree, id);
+  return chunks.length > 0 
+    ? chunks[0]
+    : null;
+}
+
+
+function getChildrenChunks(tree, id) {
+  return tree.children.filter(function(chunk) {
+    return chunk.id === id;
+  });
+}
+
+
+function unpackVertices(buf) {
+  var vertexCount = buf.length / (3 * 4);
+  var vertices = [];
+
+  for(var i=0; i<vertexCount; i++) {
+    var off = i * 3 * 4;
+    vertices.push([
+      buf.readFloatLE(off + (0 * 4)),
+      buf.readFloatLE(off + (1 * 4)),
+      buf.readFloatLE(off + (2 * 4)),
+      ]);
+  }
+
+  return vertices;
+}
+
+
+function unpackFaces(buf) {
+  var faceCount = buf.length / (3 * 2);
+  var faces = [];
+
+  for(var i=0; i<faceCount; i++) {
+    var off = i * 3 * 2;
+    faces.push([
+      buf.readUInt16LE(off + (0 * 2)),
+      buf.readUInt16LE(off + (1 * 2)),
+      buf.readUInt16LE(off + (2 * 2)),
+      ]);
+  }
+
+  return faces;
+}
+
+
 module.exports = function(buf) {
-  return parseChunk(buf, 0);
+  var rootChunk = parseChunk(buf, 0);
+
+  var editorChunk = getChildChunk(rootChunk, 0x3D3D);
+  var objectChunks = getChildrenChunks(editorChunk, 0x4000);
+
+  var objects = objectChunks.map(function(objectChunk) {
+    var triMeshChunk = getChildChunk(objectChunk, 0x4100);
+    var vertexListChunk = getChildChunk(triMeshChunk, 0x4110);
+    var faceListChunk = getChildChunk(triMeshChunk, 0x4120);
+
+    return {
+      name: objectChunk.objectName,
+      vertices: unpackVertices(vertexListChunk.vertices),
+      faces: unpackFaces(faceListChunk.faces)
+    };
+  });
+
+  return {
+    objects: objects
+  };
 };
