0092cb6a50f257b82424c32ebe9f9445e1afa426
reydus <raymondnes@hotmail.com>
2020-02-27 10:19:26 +0000

    Adding debug lines, sample .3ds

---
+++ launch.js
@@ -0,0 +1,8 @@
+var parse3DS = require('parse-3ds');
+var fs = require('fs');
+ 
+var buf = fs.readFileSync('./cubic.3ds');
+var parsed = parse3DS(buf, { 'objects':true, 'tree':false });
+
+console.log(typeof parsed)
+console.log(parsed)
